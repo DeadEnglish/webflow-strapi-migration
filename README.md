5b6b30582debf013a78896fff2e6a0b920336c95
Liam Canetti <liam.canetti@marshmallow.co>
2025-02-20 12:45:27 +0000

    feat(init): initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# blog-migration-poc
+A very basic implementation for the migration ran against a local version version of strapi.
+
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
---
+++ index.ts
@@ -0,0 +1,57 @@
+import csv from "csv-parser";
+import { createReadStream } from "node:fs";
+
+const strapiApiKey = "strapi_api_key";
+
+const faqFile = "faqs.csv";
+
+const parsedFileName = "parsedFaqs.json";
+
+function normaliseHeader(header: string) {
+  return header.replace(/ /g, "_").toLowerCase();
+}
+
+/** Function to parse CSV. currently using FAQs but can also be used for blogs */
+function parseFaqs() {
+  const result: unknown[] = [];
+
+  // read csv file
+  createReadStream(faqFile)
+    // Normalise headers to be lower case and remove spaces
+    .pipe(csv({ mapHeaders: ({ header }) => normaliseHeader(header) }))
+    // when we have a data event, push to the results array
+    .on("data", (data) => result.push(data))
+    // on stream end, write results to the file
+    // Currently we only write the first 2 results as this file is massive
+    .on("end", () => {
+      Bun.write(parsedFileName, JSON.stringify([result[0], result[1]]));
+    });
+}
+
+/** Funciton to import FAQs into strapi using the api */
+async function importFaqs() {
+  // read file
+  const file = Bun.file(parsedFileName);
+
+  // get json from file
+  const blogs = await file.json();
+
+  // using local host but obivously url can change
+  fetch("http://127.0.0.1:1337/api/faqs", {
+    method: "post",
+    headers: {
+      "content-type": "application/json",
+      Authorization: `bearer ${strapiApiKey}`,
+    },
+    body: JSON.stringify({
+      data: {
+        title: blogs[0].Name,
+        old_content: blogs[0]?.["Post Body"] || "could not find body",
+      },
+    }),
+  })
+    .then((res) => console.log(res))
+    .catch((e) => console.log(e));
+}
+
+parseFaqs();
